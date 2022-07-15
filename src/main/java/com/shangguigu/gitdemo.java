15b3aab214599d4ee919739771385734eab27af1
shenjq <1574897970@qq.com>
2022-07-15 12:30:39 +0800

    second commit

--- src/main/java/com/shangguigu/gitdemo.java
+++ src/main/java/com/shangguigu/gitdemo.java
@@ -3,5 +3,6 @@ package com.shangguigu;
 public class gitdemo {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
